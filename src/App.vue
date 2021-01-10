3e10d641880b3dccb097e6154364eed8d4c5e33f
Rubén Sánchez <rsm.ruben@gmail.com>
2021-01-10 17:16:59 +0100

    Add project files

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <tree :data="treeData" node-text="name" layoutType="horizontal" class="tree"></tree>
+    </div>
+  </div>
+</template>
+
+<script>
+import { tree } from "vued3tree";
+import json_data from "@/assets/tree.json";
+
+export default {
+  name: "App",
+  components: { tree },
+  data: function() {
+    return {
+      treeData: json_data,
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+.tree {
+    width: 100%;
+    height: 100%;
+}
+.container{
+    width: 100vw;
+    height: 100vh;
+}
+</style>
