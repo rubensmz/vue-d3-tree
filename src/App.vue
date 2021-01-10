37ff8223600de4483aa26d2e503bc0263a544c0a
Rubén Sánchez <rsm.ruben@gmail.com>
2021-01-10 17:31:34 +0100

    Add tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,32 @@
 <template>
   <div id="app">
     <div class="container">
-      <tree :data="treeData" node-text="name" layoutType="horizontal" class="tree"></tree>
+      <tree
+        :data="treeData"
+        ref="tree"
+        v-model="currentData"
+        class="tree"
+        :nodeTextDisplay="nodeTextDisplay"
+        :identifier="getId"
+        :nodeTextMargin="nodeTextMargin"
+        :zoomable="zoomable"
+        :leafTextMargin="leafTextMargin"
+        :node-text="nodeText"
+        :margin-x="Marginx"
+        :margin-y="Marginy"
+        :radius="radius"
+        :layoutType="layoutType"
+        :duration="duration"
+        :minZoom="minZoom"
+        :maxZoom="maxZoom"
+      ></tree>
     </div>
   </div>
 </template>
 
 <script>
 import { tree } from "vued3tree";
-import json_data from "@/assets/tree.json";
+import json_data from "@/assets/data/tree.json";
 
 export default {
   name: "App",
@@ -16,6 +34,23 @@ export default {
   data: function() {
     return {
       treeData: json_data,
+      type: "tree",
+      layoutType: "horizontal",
+      duration: 750,
+      Marginx: 30,
+      Marginy: 30,
+      radius: 3,
+      leafTextMargin: 6,
+      nodeTextMargin: 6,
+      nodeText: "name",
+      currentData: null,
+      zoomable: true,
+      isLoading: false,
+      nodeTextDisplay: "all",
+      linkLayout: "bezier",
+      minZoom: 0.8,
+      maxZoom: 9,
+      events: [],
     };
   },
 };
@@ -30,11 +65,11 @@ export default {
   color: #2c3e50;
 }
 .tree {
-    width: 100%;
-    height: 100%;
+  width: 100%;
+  height: 100%;
 }
-.container{
-    width: 100vw;
-    height: 100vh;
+.container {
+  width: 100vw;
+  height: 100vh;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-import Vue from 'vue'
-import App from './App.vue'
-
-Vue.config.productionTip = false
+import Vue from 'vue';
+import App from './App.vue';
+import "@/assets/styles/tailwind.css"
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
