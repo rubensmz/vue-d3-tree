0878171a57ab1aebf23ad6900bfb577411bd02e8
Rubén Sánchez <rsm.ruben@gmail.com>
2021-01-10 17:55:48 +0100

    Add sidebar and reset zoom button

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,37 @@
 <template>
-  <div id="app">
-    <div class="container">
-      <tree
-        :data="treeData"
-        ref="tree"
-        v-model="currentData"
-        class="tree"
-        :nodeTextDisplay="nodeTextDisplay"
-        :identifier="getId"
-        :nodeTextMargin="nodeTextMargin"
-        :zoomable="zoomable"
-        :leafTextMargin="leafTextMargin"
-        :node-text="nodeText"
-        :margin-x="Marginx"
-        :margin-y="Marginy"
-        :radius="radius"
-        :layoutType="layoutType"
-        :duration="duration"
-        :minZoom="minZoom"
-        :maxZoom="maxZoom"
-      ></tree>
+  <div id="app" class="w-full overflow-hidden">
+    <div class="flex">
+      <div
+        class="bg-indigo-100 rounded-md justify-center items-center flex-initial w-3/12"
+      >
+        <button
+          class="mt-3 focus:outline-none w-32 py-2 rounded-md font-semibold text-white bg-indigo-500 ring-4 ring-indigo-300"
+          @click="resetZoom"
+        >
+          Reset zoom
+        </button>
+      </div>
+      <div class="tree-container flex-1">
+        <h1 class="bg-green-500 rounded-md text-white font-bold">Title</h1>
+        <tree
+          :data="treeData"
+          ref="tree"
+          v-model="currentData"
+          class="tree"
+          :nodeTextDisplay="nodeTextDisplay"
+          :nodeTextMargin="nodeTextMargin"
+          :zoomable="zoomable"
+          :leafTextMargin="leafTextMargin"
+          :node-text="nodeText"
+          :margin-x="Marginx"
+          :margin-y="Marginy"
+          :radius="radius"
+          :layoutType="layoutType"
+          :duration="duration"
+          :minZoom="minZoom"
+          :maxZoom="maxZoom"
+        ></tree>
+      </div>
     </div>
   </div>
 </template>
@@ -53,6 +65,17 @@ export default {
       events: [],
     };
   },
+  methods: {
+    resetZoom() {
+      if (!this.$refs["tree"]) {
+        return;
+      }
+      this.isLoading = true;
+      this.$refs["tree"].resetZoom().then(() => {
+        this.isLoading = false;
+      });
+    },
+  },
 };
 </script>
 
@@ -68,7 +91,7 @@ export default {
   width: 100%;
   height: 100%;
 }
-.container {
+.tree-container {
   width: 100vw;
   height: 100vh;
 }
